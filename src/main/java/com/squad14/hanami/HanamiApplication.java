5b0070632edb863affc3e2c82f3bc18142258471
Anderson1605R <114451515+Anderson1605R@users.noreply.github.com>
2024-05-20 10:54:01 -0300

    Template inicial

---
+++ src/main/java/com/squad14/hanami/HanamiApplication.java
@@ -0,0 +1,13 @@
+package com.squad14.hanami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squad14/hanami/HanamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad14.hanami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
