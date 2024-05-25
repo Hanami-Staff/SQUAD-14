f117d0a1be639d5c473c7edee301e0c5d1d89ad2
Alessandro Farias <115547068+fariassilva@users.noreply.github.com>
2024-05-25 16:49:42 -0300

    feat: add GetAllService

---
+++ src/main/java/com/squad14/hanami/service/post/GetAllService.java
@@ -0,0 +1,22 @@
+package com.squad14.hanami.service.post;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.squad14.hanami.dto.post.PostDTO;
+
+import com.squad14.hanami.repository.PostRepository;
+
+@Service
+public class GetAllService {
+  @Autowired
+  private PostRepository postRepository;
+
+  public List<PostDTO> getAllPost(){
+    List<PostDTO> posts=postRepository.findAll().stream().map(PostDTO::new).toList();
+    return posts;
+  }
+
+}
