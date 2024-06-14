1869c4882e02604a4a096da13ba3f63d70d18399
1Jeferson <jefersonnanet.95@gmail.com>
2024-06-14 12:39:56 -0300

    refactor: update GeIdService to GetIdPostService

--- src/main/java/com/squad14/hanami/service/post/UpdateService.java
+++ src/main/java/com/squad14/hanami/service/post/UpdateService.java
@@ -1,6 +1,5 @@
 package com.squad14.hanami.service.post;
 
-
 import com.squad14.hanami.dto.post.UpdatePostDTO;
 import com.squad14.hanami.model.Post;
 import com.squad14.hanami.repository.PostRepository;
@@ -13,12 +12,12 @@ import java.util.UUID;
 public class UpdateService {
 
     @Autowired
-    private GetIdService getIdService;
+    private GetIdPostService getIdService;
 
     @Autowired
     private PostRepository postRepository;
 
-    public String updatePost(UUID id, UpdatePostDTO post){
+    public String updatePost(UUID id, UpdatePostDTO post) {
         Post updatedPost = getIdService.getPostById(id).get();
         post.title().ifPresent(updatedPost::setTitle);
         post.content().ifPresent(updatedPost::setContent);
