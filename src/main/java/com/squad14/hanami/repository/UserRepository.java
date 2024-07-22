059cf39bd771550fdc811e00ba642a6396b85909
RabbitDeWitt <davidsocoelho@outlook.com>
2024-07-22 16:22:21 -0300

    feat: add UserRepository

---
+++ src/main/java/com/squad14/hanami/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.squad14.hanami.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.squad14.hanami.model.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
