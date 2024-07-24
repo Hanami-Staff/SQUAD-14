0b16382efd4fb626f2ab7a72e59d55d20afa7d7a
RabbitDeWitt <davidsocoelho@outlook.com>
2024-07-24 16:45:59 -0300

    reafactor: add userId and username in PostDTO

--- src/main/java/com/squad14/hanami/dto/post/PostDTO.java
+++ src/main/java/com/squad14/hanami/dto/post/PostDTO.java
@@ -5,8 +5,9 @@ import com.squad14.hanami.model.Post;
 import java.util.Date;
 import java.util.UUID;
 
-public record PostDTO(UUID id, String title, String content, Date createdAt) {
-    public PostDTO(Post post){
-        this(post.getId(), post.getTitle(), post.getContent(), post.getCreatedAt());
+public record PostDTO(UUID id, UUID userId, String username, String title, String content, Date createdAt) {
+    public PostDTO(Post post) {
+        this(post.getId(), post.getUser().getId(), post.getUser().getName(), post.getTitle(), post.getContent(),
+                post.getCreatedAt());
     }
 }
