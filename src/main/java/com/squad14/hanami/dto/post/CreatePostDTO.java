2750c992e29769d807e24ed45462266d5a5709e1
RabbitDeWitt <davidsocoelho@outlook.com>
2024-07-24 16:45:40 -0300

    reafactor: add userId in CreatePostDTO

--- src/main/java/com/squad14/hanami/dto/post/CreatePostDTO.java
+++ src/main/java/com/squad14/hanami/dto/post/CreatePostDTO.java
@@ -1,9 +1,11 @@
 package com.squad14.hanami.dto.post;
 
+import java.util.UUID;
+
 import com.squad14.hanami.model.Post;
 
-public record CreatePostDTO(String title, String content) {
-    public CreatePostDTO(Post post){
-        this(post.getTitle(), post.getContent());
+public record CreatePostDTO(UUID userId, String title, String content) {
+    public CreatePostDTO(Post post) {
+        this(post.getUser().getId(), post.getTitle(), post.getContent());
     }
 }
