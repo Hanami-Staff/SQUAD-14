9b035db9455e35f431421b75813174dfabf188ef
RabbitDeWitt <davidsocoelho@outlook.com>
2024-08-03 19:05:12 -0300

    feat: add findByUserId in postRepository

--- src/main/java/com/squad14/hanami/repository/PostRepository.java
+++ src/main/java/com/squad14/hanami/repository/PostRepository.java
@@ -3,8 +3,9 @@ package com.squad14.hanami.repository;
 import com.squad14.hanami.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
-
+  List<Post> findByUserId(UUID userId);
 }
